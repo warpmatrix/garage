cceedd2059854d205a49a2368b68a154b0aa747e
warpmatrix <warpmatrixlyk@gmail.com>
2024-03-20 10:59:48 +0800

    tools(cmake): split include and source

--- tools/cmake/main.cpp
+++ tools/cmake/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "f1.hpp"
 
 int main(int argc, char const *argv[]) {
-    std::cout << "hello" << '\n';
+    std::cout << "hello in main function" << '\n';
+    f1();
     return 0;
 }
---
+++ tools/cmake/src/f1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void f1() {
+    std::cout << "hello in f1" << '\n';
+}
